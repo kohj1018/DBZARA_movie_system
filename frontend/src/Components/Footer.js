1348a785337ae3432976d05f124dba805c2e3f89
jaehun <wognskec@hanyang.ac.kr>
2021-11-03 00:43:35 +0900

    MoviePoster.js : 부모 사이즈게 맡게 수정 &&  hover 적용
    Home.js : index page 구조 잡기 (yes24 clone)
    header (항목설정 0, 세부항목x)
    FirstImg
    Ranking
    BestPlay
    Event (x)
    footer (x)

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -2,17 +2,30 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.footer`
-  position: absolute;
-  bottom: 50px;
-  height: 50px;
-  width: 100%;
-  background-color: ;
+  padding-bottom: 40px;
+  height: 420px;
+  background-color: RGB(191, 189, 97);
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+`;
+
+const Div1 = styled.div`
+  height: 180px;
+`;
+
+const Div2 = styled.div`
+  height: 137px;
 `;
 
 const Footer = () => {
   return (
     <>
-      <Container>footer</Container>
+      <Container>
+        <Div1>할인권 다운로드</Div1>
+        <Div2>footer</Div2>
+      </Container>
     </>
   );
 };
---
+++ frontend/src/Routes/Event.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Button, Typography } from "@material-ui/core";
+
+const Container = styled.div`
+  margin-top: 50px;
+  padding: 15px;
+  color: blue;
+  display: flex;
+`;
+
+const Btn = styled(Button)`
+  && {
+    color: blue;
+    size: medium;
+  }
+`;
+const Demo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TypoGraphy = styled(Typography)`
+  && {
+  }
+`;
+
+const Event = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Container>
+        <Btn variant="contained">material</Btn>
+        <Demo>
+          <TypoGraphy variant="subtitle1" component="div">
+            대화창 만들기
+          </TypoGraphy>
+
+          <Btn variant="outlined" onClick={() => setOpen(true)}>
+            open
+          </Btn>
+        </Demo>
+        {/* <SimpleDialog /> */}
+      </Container>
+    </>
+  );
+};
+
+// const SimpleDialog = ({, open}) => {};
+
+export default Event;
