ea8d759738e6a47ab578da9fcbabed009a4143c3
MinJunsu <sunsuking@gmail.com>
2021-11-08 01:17:51 +0900

    Add Social Login

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from accounts.models import User, Employee, Department
+from accounts.models import User, Employee, Department, Profile, Grade
 # Register your models here.
 
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
     pass
 
 
