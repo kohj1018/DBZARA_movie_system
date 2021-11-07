5e850d72e2efe90e3572fc5b751be432a6cfa716
MinJunsu <sunsuking@gmail.com>
2021-11-07 21:24:34 +0900

    fix accounts model - Department

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from accounts.models import User
+from accounts.models import User, Employee, Department
 # Register your models here.
 
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     pass
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'full_department_name', 'department']
