0b6812808136840cd772009ac176eb015ba4ef9c
MinJunsu <sunsuking@gmail.com>
2021-11-12 23:35:49 +0900

    Create Movie List APIView(GET)

--- backend/movie/urls.py
+++ backend/movie/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'movie'
 
 urlpatterns = [
-    path('actor/', views.ActorCreateView.as_view(), name='actor-create'),
     path('movies/', views.MovieListView.as_view(), name='movie-list'),
     path('actors/', views.ActorListView.as_view(), name='actor-list'),
-    path('directors/', views.DirectorListView.as_view(), name='director-list')
+    path('directors/', views.DirectorListView.as_view(), name='director-list'),
+
+    path('api/movies/', views.MovieAPIView.as_view(), name='movie-list')
 ]
--- backend/movie/views.py
+++ backend/movie/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 from .models import Movie, Actor, Director
-
-
+from .serializers import MovieSerializer
 # Create your views here.
+
+
 class MovieListView(ListView):
     model = Movie
     context_object_name = 'movies'
@@ -25,3 +29,9 @@ class DirectorListView(ListView):
     template_name = 'movie/person_list.html'
     paginate_by = 12
 
+
+class MovieAPIView(APIView):
+    def get(self, request):
+        query_set = Movie.objects.all()
+        serializer = MovieSerializer(query_set, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
