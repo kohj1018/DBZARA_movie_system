cb30e399fc33759e52cdfa461717af673967cb15
YunHo Choi <choi15ho@gmail.com>
2021-11-12 19:23:32 +0900

    Modify create_employees file

--- backend/accounts/management/commands/create_employees.py
+++ backend/accounts/management/commands/create_employees.py
@@ -1,28 +1,38 @@
-from random import randint, random, choice
-from datetime import timedelta, datetime
+from random import randint, choice
 import json
 
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
-from django.db.models import Q
-from django_seed import Seed
 
 from accounts.models import Employee, Department
 from cinema.models import Cinema
 
 
+def create_department():
+    with open('accounts/management/commands/departments.json', encoding='UTF-8-SIG') as json_file:
+        departments = json.load(json_file)
+        count = 0
+        for department in departments:
+            count += 1
+            Department.objects.get_or_create(
+                id=department['pk'],
+                defaults={
+                    'name': department['fields']['name'],
+                    'department': None if department['fields']['department'] is None else Department.objects.get(pk=int(department['fields']['department']))
+                }
+            )
+        print(f'-- 부서 데이터 {count}개 생성 완료 --')
+    return Department.objects.exclude(department__isnull=True)
+
+
 class Command(BaseCommand):
     help = 'this command create employee'
-
-    # def add_arguments(self, parser):
-    #     parser.add_argument('--total', default=1, type=int)
-    #     parser.add_argument('--department', default=1, type=int)
 
     def handle(self, *args, **options):
         total = options.get('total')
         all_user = get_user_model().objects.exclude(is_staff=True)
         all_cinema = Cinema.objects.all()
-        departments = self.create_department()
+        departments = create_department()
         cinemas = sum([[element] * randint((7 - element.grade) * 10, (8 - element.grade) * 10) for element in all_cinema], [])
 
         print(len(cinemas))
@@ -34,6 +44,7 @@ class Command(BaseCommand):
                     "cinema": cinema,
                     "belong": choice(departments),
                     "register_date": user.date_joined,
+                    # TODO: salary는 belong마다 상한 하한을 정해 추가한다.
                     "salary": 0
                 }
             )
@@ -45,18 +56,3 @@ class Command(BaseCommand):
         # self.stdout.write(self.style.SUCCESS(
         #     f"{total} users have been created."))
 
-    def create_department(self):
-        with open('accounts/management/commands/departments.json', encoding='UTF-8-SIG') as json_file:
-            departments = json.load(json_file)
-            count = 0
-            for department in departments:
-                count += 1
-                Department.objects.get_or_create(
-                    id=department['pk'],
-                    defaults={
-                        'name': department['fields']['name'],
-                        'department': None if department['fields']['department'] is None else Department.objects.get(pk=int(department['fields']['department']))
-                    }
-                )
-            print(f'-- 부서 데이터 {count}개 생성 완료 --')
-        return Department.objects.exclude(department__isnull=True)
