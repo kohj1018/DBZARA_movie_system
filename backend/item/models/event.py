c8904f63505e5706d9da16a482b8efdc26102e98
MinJunsu <sunsuking@gmail.com>
2021-12-04 17:10:00 +0900

    Create Event functions & fix collect functions

--- backend/item/models/event.py
+++ backend/item/models/event.py
@@ -24,12 +24,21 @@ class Benefit(models.Model):
     def point(self):
         return self.content + int(self.item.price / 20) if self.type == 3 else int(self.item.price / 20)
 
+    @classmethod
+    def create(cls, item, category, content):
+        cls.objects.create(
+            item=item,
+            type=category,
+            content=content
+        )
+
 
 class Event(models.Model):
     title = models.CharField(max_length=50)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
-    image = models.ImageField(upload_to='item/%Y/%m', null=True)
+    poster = models.ImageField(upload_to='item/%Y/%m', null=True)
+    backdrop = models.ImageField(upload_to='item/%Y/%m', null=True)
     coupon = models.ManyToManyField(Benefit, through='Coupon', through_fields=('event', 'benefit'), related_name='+')
     non_coupon = models.ManyToManyField(Benefit, through='NonCoupon', through_fields=('event', 'benefit'), related_name='+')
 
@@ -40,8 +49,25 @@ class Coupon(models.Model):
     amount = models.IntegerField(verbose_name='최대 수량')
     duration = models.IntegerField(verbose_name='유효 기간')
 
+    @classmethod
+    def create(cls, benefit, event, amount=1000, duration=7):
+        cls.objects.create(
+            benefit=benefit,
+            event=event,
+            amount=amount,
+            duration=duration
+        )
+
 
 class NonCoupon(models.Model):
     benefit = models.ForeignKey(Benefit, on_delete=models.CASCADE, related_name='+')
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='+')
     amount = models.IntegerField(verbose_name='최대 수량')
+
+    @classmethod
+    def create(cls, benefit, event, amount=1000):
+        cls.objects.create(
+            benefit=benefit,
+            event=event,
+            amount=amount
+        )
