871d3a4a64a2804a0fa9b63a61b8425893a1b69c
YouKnowGit <choi15ho@gmail.com>
2021-12-06 04:59:59 +0900

    Create manager page cinema/stock view

--- backend/cinema/urls.py
+++ backend/cinema/urls.py
@@ -7,4 +7,5 @@ app_name = 'cinema'
 urlpatterns = [
     path('cinemas/', views.cinemas, name='cinema-list'),
     path('schedule/', views.cinema_schedule_list_view, name='schedule'),
+    path('stock/', views.cinema_stock_list_view, name='stock-list'),
 ]
