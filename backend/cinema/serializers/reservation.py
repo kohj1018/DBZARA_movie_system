cd765149a16e2df9717565320d1a0c8d07e48e03
MinJunsu <sunsuking@gmail.com>
2021-12-07 02:52:19 +0900

    Create & Fix Schedule API - Create Schedule Post Method - Fix Schedule Movie Detail

---
+++ backend/cinema/serializers/reservation.py
@@ -0,0 +1,9 @@
+from cinema.serializers.base import *
+
+
+class ReservationSerializer(ScheduleSerializer):
+    name = serializers.ReadOnlyField(source='theater.name')
+    floor = serializers.ReadOnlyField(source='theater.floor')
+
+    class Meta(ScheduleSerializer.Meta):
+        fields = ['id', 'name', 'floor', 'datetime']
