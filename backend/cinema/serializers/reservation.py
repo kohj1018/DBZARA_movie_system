1d805bf3e47930277af1b5703e46d44bb46b667a
MinJunsu <sunsuking@gmail.com>
2021-12-08 00:24:01 +0900

    Fix Reservation API - add Seat data

--- backend/cinema/serializers/reservation.py
+++ backend/cinema/serializers/reservation.py
@@ -4,6 +4,7 @@ from cinema.serializers.base import *
 class ReservationSerializer(ScheduleSerializer):
     name = serializers.ReadOnlyField(source='theater.name')
     floor = serializers.ReadOnlyField(source='theater.floor')
+    seat = SeatSerializer(source='theater.seat')
 
     class Meta(ScheduleSerializer.Meta):
-        fields = ['id', 'name', 'floor', 'start_datetime', 'end_datetime']
+        fields = ['id', 'name', 'floor', 'seat', 'start_datetime', 'end_datetime']
